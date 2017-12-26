1e17ab95de4da7bf2aa0b04f0ecfe45abf38da48
Радченко Марина Олеговна <marinika0309@gmail.com>
2017-12-26 22:44:56 +0500

    V4

--- v2/Net.Core/Model_Of_Shlak/Model_Of_Shlak/Controllers/HomeController.cs
+++ v2/Net.Core/Model_Of_Shlak/Model_Of_Shlak/Controllers/HomeController.cs
@@ -8,11 +8,19 @@ using Model_Of_Shlak.Models;
 using Microsoft.SolverFoundation;
 using Microsoft.SolverFoundation.Services;
 using System.Xml;
+//using Excel = Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
 
 namespace Model_Of_Shlak.Controllers
 {
     public class HomeController : Controller
     {
+
+        Microsoft.Office.Interop.Excel.Application application;
+        Microsoft.Office.Interop.Excel.Workbook workBook;
+        Microsoft.Office.Interop.Excel.Worksheet worksheet;
         public IActionResult Index()
         {
             RashetShlak cls = new RashetShlak();
@@ -83,7 +91,7 @@ namespace Model_Of_Shlak.Controllers
                 Decision e = new Decision(Domain.Real, "e");
                 Decision f = new Decision(Domain.Real, "f");
 
-                Model model = context.CreateModel();
+                Microsoft.SolverFoundation.Services.Model model = context.CreateModel();
 
                 model.AddDecisions(a, b, c, d, e, f);
 
@@ -110,13 +118,13 @@ namespace Model_Of_Shlak.Controllers
             double[] temps = { cls.TempVhod, cls.TempExit };
             double[,] koefs = new double[2, 3];
 
-            for (int i = 0; i < temps.GetLength(0); i++)
+            for (int i = 0; i < 2; i++)
             {
                 Decision a = new Decision(Domain.Real, "a");
                 Decision b = new Decision(Domain.Real, "b");
                 Decision c = new Decision(Domain.Real, "c");
 
-                Model model = context.CreateModel();
+                Microsoft.SolverFoundation.Services.Model model = context.CreateModel();
 
                 model.AddDecisions(a, b, c);
 
@@ -156,8 +164,8 @@ namespace Model_Of_Shlak.Controllers
             }
             #endregion 
 
-            cls.VisVhod = koefs[0, 2] + koefs[0, 0] * Math.Pow(cls.FeOVhod, 1) + koefs[0, 1] * cls.FeOVhod * cls.FeOVhod;
-            cls.VisExit = koefs[1, 2] + koefs[1, 0] * Math.Pow(cls.FeOExit, 1) + koefs[1, 1] * cls.FeOExit * cls.FeOExit;
+            cls.VisVhod = koefs[0, 2] + koefs[0, 0] * cls.FeOVhod + koefs[0, 1] * cls.FeOVhod * cls.FeOVhod;
+            cls.VisExit = koefs[1, 2] + koefs[1, 0] * cls.FeOExit + koefs[1, 1] * cls.FeOExit * cls.FeOExit;
             cls.VisOcnVhod = cls.VisVhod + 196*(cls.Osnovnoct -1);
             cls.VisOsnExit = cls.VisExit + 4 * (cls.Osnovnoct - 1);
             cls.KoffB = (Math.Log10(Math.Log10(cls.VisOsnExit)) - Math.Log10(Math.Log10(cls.VisOcnVhod))) / (cls.TempExit - cls.TempVhod);
@@ -167,14 +175,69 @@ namespace Model_Of_Shlak.Controllers
             cls.Vis1250 = Math.Pow(10, Math.Pow(10, cls.KoffA + cls.KoffB * 1250));
             cls.Vis1275 = Math.Pow(10, Math.Pow(10, cls.KoffA + cls.KoffB * 1275));
             cls.Vis1300 = Math.Pow(10, Math.Pow(10, cls.KoffA + cls.KoffB * 1300));
-            cls.TempKristal = (cls.KoffA - Math.Log10(Math.Log10(25))) / -cls.KoffB;
+            cls.TempKristal = (cls.KoffA - Math.Log10(Math.Log10(25))) / ((-1)*cls.KoffB);
 
+
+            #region Excel  
+            
+            // Открываем приложение
+            application = new Application
+            {
+                DisplayAlerts = false
+            };
+
+            // Файл шаблона
+            const string template = "shlak2.xlsx";
+
+            // Открываем книгу
+            workBook = application.Workbooks.Open(Path.Combine(Environment.CurrentDirectory, template));
+
+            // Получаем активную таблицу
+            worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.Worksheets[1];//.get_Item(2);            
+            // Записываем данные
+            worksheet.Range["C22"].Value = cls.TempVhod;
+            worksheet.Range["C23"].Value = cls.FeOVhod;
+            worksheet.Range["C24"].Value = cls.TempExit;
+            worksheet.Range["C25"].Value = cls.FeOExit;
+            worksheet.Range["C26"].Value = cls.Osnovnoct;
+            //ВВОД данных и р-т
+            double VisVhodEX = double.Parse(worksheet.Range["C27"].Value.ToString());
+            double VisExitEX = double.Parse(worksheet.Range["C28"].Value.ToString());
+            double VisOcnVhodEX = double.Parse(worksheet.Range["C29"].Value.ToString());
+            double VisOsnExitEX = double.Parse(worksheet.Range["C30"].Value.ToString());
+            double KoffAEX = double.Parse(worksheet.Range["C31"].Value.ToString());
+            double KoffBEX = double.Parse(worksheet.Range["C32"].Value.ToString());
+            double Vis1180EX = double.Parse(worksheet.Range["C33"].Value.ToString());
+            double Vis1200EX = double.Parse(worksheet.Range["C34"].Value.ToString());
+            double Vis1250EX = double.Parse(worksheet.Range["C35"].Value.ToString());
+            double Vis1275EX = double.Parse(worksheet.Range["C36"].Value.ToString());
+            double Vis1300EX = double.Parse(worksheet.Range["C37"].Value.ToString());
+            double TempKristalEX = double.Parse(worksheet.Range["C38"].Value.ToString());
+            // Показываем приложение
+            
+            workBook.Save();
+
+            cls.pogr =   Math.Round((((Math.Abs(((Math.Abs(VisVhodEX) - Math.Abs(cls.VisVhod)) / Math.Abs(VisVhodEX))) +
+                                    Math.Abs(((Math.Abs(VisExitEX) - Math.Abs(cls.VisExit)) / Math.Abs(VisExitEX)))+
+                                    Math.Abs(((Math.Abs(VisOcnVhodEX) - Math.Abs(cls.VisOcnVhod)) / Math.Abs(VisOcnVhodEX))) +
+                                    Math.Abs(((Math.Abs(VisOsnExitEX) - Math.Abs(cls.VisOsnExit)) / Math.Abs(VisOsnExitEX))) +
+                                    Math.Abs(((Math.Abs(Vis1250EX) - Math.Abs(cls.Vis1250)) / Math.Abs(Vis1250EX))) +
+                                    Math.Abs(((Math.Abs(Vis1275EX) - Math.Abs(cls.Vis1275)) / Math.Abs(Vis1275EX)))+
+                                    Math.Abs(((Math.Abs(Vis1300EX) - Math.Abs(cls.Vis1300)) / Math.Abs(Vis1300EX))) +
+                                    Math.Abs(((Math.Abs(TempKristalEX) - Math.Abs(cls.TempExit)) / Math.Abs(TempKristalEX))) +
+                                    Math.Abs(((Math.Abs(KoffAEX) - Math.Abs(cls.KoffA)) / Math.Abs(KoffAEX))) +
+                                    Math.Abs(((Math.Abs(KoffBEX) - Math.Abs(cls.KoffB)) / Math.Abs(KoffBEX))) +
+                                    Math.Abs(((Math.Abs(Vis1180EX) - Math.Abs(cls.Vis1180)) / Math.Abs(Vis1180EX))) +
+                                    Math.Abs(((Math.Abs(Vis1200EX) - Math.Abs(cls.Vis1200)) / Math.Abs(Vis1200EX))))) /12)*100,2);
+
+            #endregion
+            workBook.Close(false, Type.Missing, Type.Missing);
+            application.Quit();
             return View();
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+        
+        
     }
 }
--- v2/Net.Core/Model_Of_Shlak/Model_Of_Shlak/Models/RashetShlak.cs
+++ v2/Net.Core/Model_Of_Shlak/Model_Of_Shlak/Models/RashetShlak.cs
@@ -7,11 +7,11 @@ namespace Model_Of_Shlak.Models
 {
     public class RashetShlak
     {
-        public double TempVhod { get; set; } = 1180;
-        public double FeOVhod { get; set; } = 9;
+        public double TempVhod { get; set; } = 1200;
+        public double FeOVhod { get; set; } = 10;
         public double TempExit { get; set; } = 1300;
-        public double Osnovnoct { get; set; } = 1.2;
-        public double FeOExit { get; set; } = 4;
+        public double Osnovnoct { get; set; } = 1.4;
+        public double FeOExit { get; set; } = 5;
 
 
         public double VisVhod { get; set; }
@@ -26,6 +26,8 @@ namespace Model_Of_Shlak.Models
         public double Vis1275 { get; set; }
         public double Vis1300 { get; set; }
         public double TempKristal { get; set; }
+
+        public double pogr { get; set; }
 
         public RashetShlak()
         {
